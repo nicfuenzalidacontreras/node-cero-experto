c412eeb65657217d6c276d349a373a47021894a1
Nicolás Fuenzalida <na.fuenzalida.contreras@gmail.com>
2023-04-23 20:08:00 -0400

    tareas por hacer parte uno

---
+++ 05-tareas-hacer/app.js
@@ -0,0 +1,14 @@
+require('colors');
+const { inquirerMenu, pausa } = require('./helpers/inquirer');
+
+const main = async() => {
+    let opt = '';
+
+    do {
+        opt = await inquirerMenu();
+        console.log({opt});
+        await pausa;
+    } while ( opt !== '0');
+}
+
+main();
