6064194593e3091620492333c830fa8e3f66f0a0
Nicolás Fuenzalida <na.fuenzalida.contreras@gmail.com>
2023-04-25 20:24:47 -0400

    tareas por hacer parte dos

--- 05-tareas-hacer/app.js
+++ 05-tareas-hacer/app.js
@@ -1,14 +1,67 @@
 require('colors');
-const { inquirerMenu, pausa } = require('./helpers/inquirer');
+
+const { guardarDB, leerDB } = require('./helpers/guardarArchivo');
+const { inquirerMenu, 
+    pausa,
+    leerInput,
+    listadoTareasBorrar,
+    confirmar,
+    mostrarListadoChecklist
+} = require('./helpers/inquirer');
+const Tareas = require('./models/tareas');
 
 const main = async() => {
     let opt = '';
+    const tareas = new Tareas();
+
+    const tareasDB = leerDB();
+
+    if ( tareasDB ) {
+        tareas.cargarTareasFromArray( tareasDB );
+    }
 
     do {
         opt = await inquirerMenu();
-        console.log({opt});
-        await pausa;
-    } while ( opt !== '0');
+
+        switch (opt) {
+            case '1':
+                const desc = await leerInput('Descripción:');
+                tareas.crearTarea( desc );
+            break;
+
+            case '2':
+                 tareas.listadoCompleto();
+            break;  
+            
+            case '3':
+                tareas.listarPendientesCompletadas(true);
+            break;
+
+            case '4':
+                tareas.listarPendientesCompletadas(false);
+            break;
+
+            case '5':
+                const ids = await mostrarListadoChecklist( tareas.listadoArr );
+                tareas.toggleCompletadas( ids );
+            break;
+
+            case '6':
+                const id = await listadoTareasBorrar( tareas.listadoArr );
+                if ( id !== '0' ) {
+                    const ok = await confirmar('¿Está seguro?');
+                    if ( ok ) {
+                        tareas.borrarTarea( id );
+                        console.log('Tarea borrada');
+                    }
+                }
+            break;
+        }
+
+        guardarDB( tareas.listadoArr );
+
+        await pausa();
+    } while( opt !== '0' );
 }
 
 main();
--- 05-tareas-hacer/helpers/inquirer.js
+++ 05-tareas-hacer/helpers/inquirer.js
@@ -65,7 +65,93 @@ const pausa = async() => {
     await inquirer.prompt(question);
 }
 
+const leerInput = async( message ) => {
+    const question = [
+        {
+            type: 'input',
+            name: 'desc',
+            message,
+            validate( value ) {
+                if( value.length === 0 ) {
+                    return 'Por favor ingrese un valor';
+                }
+                return true;
+            }
+        }
+    ];
+
+    const { desc } = await inquirer.prompt(question);
+    return desc;
+}
+
+const listadoTareasBorrar = async( tareas = [] ) => {
+    const choices = tareas.map( (tarea, i) => {
+        const idx = `${i + 1}.`.green;
+        return {
+            value: tarea.id,
+            name:  `${ idx } ${ tarea.desc }`
+        }
+    });
+
+    choices.unshift({
+        value: '0',
+        name: '0.'.green + ' Cancelar'
+    });
+
+    const preguntas = [
+        {
+            type: 'list',
+            name: 'id',
+            message: 'Borrar',
+            choices
+        }
+    ]
+
+    const { id } = await inquirer.prompt(preguntas);
+    return id;
+}
+
+const confirmar = async(message) => {
+    const question = [
+        {
+            type: 'confirm',
+            name: 'ok',
+            message
+        }
+    ];
+
+    const { ok } = await inquirer.prompt(question);
+    return ok;
+} 
+
+const mostrarListadoChecklist = async( tareas = [] ) => {
+    const choices = tareas.map( (tarea, i) => {
+        const idx = `${i + 1}.`.green;
+        return {
+            value: tarea.id,
+            name:  `${ idx } ${ tarea.desc }`,
+            checked: ( tarea.completadoEn ) ? true : false
+        }
+    });
+
+    const pregunta = [
+        {
+            type: 'checkbox',
+            name: 'ids',
+            message: 'Selecciones',
+            choices
+        }
+    ]
+
+    const { ids } = await inquirer.prompt(pregunta);
+    return ids;
+}
+
 module.exports = {
     inquirerMenu,
     pausa,
+    leerInput,
+    listadoTareasBorrar,
+    confirmar,
+    mostrarListadoChecklist
 }
