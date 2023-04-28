913649c75d44ed3b55d3de13e316f1670fefd318
Nicolás Fuenzalida <na.fuenzalida.contreras@gmail.com>
2023-04-28 17:40:16 -0400

    webserver parte dos

--- 07-webserver/app.js
+++ 07-webserver/app.js
@@ -2,16 +2,18 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req,res) => {
-    res.send('Home');
+app.use(express.static('public'));
+
+app.get('/generic', (req, res) => {
+    res.sendFile(__dirname+'/public/generic.html');
 });
 
-app.get('/hola-mundo', (req,res) => {
-    res.send('Hola mundo');
-})
+app.get('/elements', (req, res) => {
+    res.sendFile(__dirname+'/public/elements.html');
+});
 
 app.get('*', (req, res) => {
-    res.send('404 | Page not found')
+    res.sendFile(__dirname+'/public/404.html');
 });
 
 app.listen(port, () => {
