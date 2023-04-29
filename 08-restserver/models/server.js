f8387ef5b25fcdf16f416bfffa75d6ddfc775f8d
Nicolás Fuenzalida <na.fuenzalida.contreras@gmail.com>
2023-04-29 16:17:02 -0400

    restserver parte dos

--- 08-restserver/models/server.js
+++ 08-restserver/models/server.js
@@ -5,6 +5,7 @@ class Server {
     constructor() {
         this.app  = express();
         this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
 
         this.middlewares();
 
@@ -13,33 +14,12 @@ class Server {
 
     middlewares() {
         this.app.use(cors());
+        this.app.use(express.json());
         this.app.use(express.static('public'));
     }
 
     routes() {
-        this.app.get('/api', (req, res) => {
-            res.status(200).json({
-                "msg": "get API",
-            })
-        });
-
-        this.app.put('/api', (req, res) => {
-            res.status(200).json({
-                "msg": "put API",
-            })
-        });
-
-        this.app.post('/api', (req, res) => {
-            res.status(201).json({
-                "msg": "post API",
-            })
-        });
-
-        this.app.delete('/api', (req, res) => {
-            res.status(200).json({
-                "msg": "delete API",
-            })
-        });
+        this.app.use( this.usuariosPath, require('../routes/usuarios'));
     }
 
     listen() {
